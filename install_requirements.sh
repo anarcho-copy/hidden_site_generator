5d3a8031e7c277edb81a847a860db4a18f777e20
anarchocopy <anarchocopy@protonmail.com>
2020-09-13 20:10:47 +0200

    add option for fedora

---
+++ install_requirements.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+function guide() {
+echo "usage: ./`basename $0` [debian | rhel]"
+echo "use debian option to apt based distros, such as Ubuntu."
+echo -e "use rhel option to dnf based distros, such as Fedora.\n"
+echo "this program tested under debian and fedora."
+exit 0;
+}
+
+debian(){
+apt-get update && \
+    apt-get -y install \
+    ghostscript imagemagick libimage-exiftool-perl sqlite3 coreutils nginx pdfinfo
+}
+
+
+rhel(){
+dnf install -y \
+    ghostscript ImageMagick perl-Image-ExifTool sqlite coreutils nginx poppler-utils podman-docker
+}
+
+if [ "$#" -lt 1 ]; then
+    guide;
+fi
+
+$1
