eee2d233a23f15b30149ac299735971877c21933
anarchocopy <anarchocopy@protonmail.com>
2020-09-17 18:42:08 +0200

    update id_tool.sh

--- var/id_tool.sh
+++ var/id_tool.sh
@@ -2,11 +2,14 @@
 #perl-Digest-SHA perl-IO-Compress
 #exiftool -m -DocumentID= *
 
+cd "$(dirname "$0")";
+. ../bin/config.sh #get pdf_dir
+
 function guide() {
-echo "usage: ./`basename $0` [generate | list | new]"
+echo -e "usage: ./`basename $0` [generatedb] [listdb] [new <file>] \n  [set <uuid:xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx> <file>]"
 }
 
-function generate() {
+function generatedb() {
 count=$(sqlite3 books.db "SELECT COUNT(*) FROM books;");
 for i in `seq 1 $count`;
 do
@@ -15,24 +18,31 @@ do
     sqlite3 books.db "UPDATE books SET id='uuid:$id' WHERE rowid='$i';"
     echo "$i  >  $id"
     url=$(sqlite3 books.db "SELECT url FROM books WHERE rowid='$i';")
-    exiftool -DocumentID="uuid:$id" /var/www/public/anarcho-copy.org/free/$url.pdf
-    echo "/var/www/public/anarcho-copy.org/free/$url.pdf"
+    exiftool -m -DocumentID="uuid:$id" $pdf_dir/$url.pdf
+    echo "$pdf_dir/$url.pdf"
     echo "----------------------------------------------------------------"
 done
 }
 
-function list() {
+function listdb() {
 #uuidgen look this
 sqlite3 books.db "SELECT id,title FROM books;";
 }
 
 function new() {
-echo "uuid:$(cat /proc/sys/kernel/random/uuid)"
-date '+%d/%m/%Y %H:%M:%S'
+id=$(cat /proc/sys/kernel/random/uuid);
+exiftool -m -DocumentID="uuid:$id" $1 && exiftool $1 | grep 'Document ID';
+echo "uuid:$id";
+echo "$(date '+%d/%m/%Y %H:%M:%S')  <  $1";
+}
+
+function set() {
+exiftool -m -DocumentID="$1" $2 && exiftool $2 | grep 'Document ID';
+echo "$1 - $2"
 }
 
 if [ "$#" -lt 1 ]; then
     guide;
 fi
 
-$1;
+$@;
