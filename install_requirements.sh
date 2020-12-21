4771fcdcf745c02db2f45fd46d70c0f02d999eed
anarchocopy <anarchocopy@protonmail.com>
2020-12-21 22:12:19 +0100

    update install_requirements.sh

--- install_requirements.sh
+++ install_requirements.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# these scripts written in GNU bash; version 5.0.16(1)-release (x86_64-pc-linux-gnu)
+#  and version 5.0.17(1)-release (x86_64-redhat-linux-gnu)
+#  and version 4.4.19(1)-release (x86_64-redhat-linux-gnu)
 
 function guide() {
 echo "usage: ./`basename $0` [debian | rhel]"
@@ -11,14 +14,13 @@ exit 0;
 debian(){
 apt-get update && \
     apt-get install \
-    ghostscript imagemagick libimage-exiftool-perl sqlite3 coreutils nginx pdfinfo
+    ghostscript imagemagick libimage-exiftool-perl sqlite3 coreutils nginx pdfinfo bc
 	# libio-compress-perl libdigest-sha-perl
 }
 
-
 rhel(){
 dnf install \
-    ghostscript ImageMagick perl-Image-ExifTool sqlite coreutils nginx poppler-utils podman-docker perl-IO-Compress perl-Digest-SHA
+    ghostscript ImageMagick perl-Image-ExifTool sqlite coreutils nginx poppler-utils podman-docker perl-IO-Compress perl-Digest-SHA bc
 }
 
 if [ "$#" -lt 1 ]; then
